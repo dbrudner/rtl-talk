8c2ffd008ae933f5cf02d7e4c58b03256ade9aaa
Dave Brudner <dbrudne1@gmail.com>
2020-01-26 15:12:03 -0600

    Updates

---
+++ precache-manifest.d78e9e80dbc7a486f86bc91415fb700b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4594f2751c58e1bc499",
+    "url": "/rtl-talk/static/js/runtime~main.f4594f27.js"
+  },
+  {
+    "revision": "314cbbe63f30e4a33c5f",
+    "url": "/rtl-talk/static/js/main.314cbbe6.chunk.js"
+  },
+  {
+    "revision": "5bff422aeae3eef16f94",
+    "url": "/rtl-talk/static/js/1.5bff422a.chunk.js"
+  },
+  {
+    "revision": "5bff422aeae3eef16f94",
+    "url": "/rtl-talk/static/css/1.a79ab582.chunk.css"
+  },
+  {
+    "revision": "059b5a8732110c32fc29a838afc15309",
+    "url": "/rtl-talk/index.html"
+  }
+];
